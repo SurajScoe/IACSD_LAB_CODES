8ad74ac3aa2a9647d5c4c36abc878221f2d0f8b6
suraj <surajmalge123@gmail.com>
2023-10-19 18:32:47 +0530

    done

--- c++_Labs/cpp_Lab_Day_5/Inheritance/GeometricShapes.cpp
+++
@@ -1,7 +0,0 @@
-/*--Geometric Shapes with Polymorphism:
-Problem Statement: Extend the shape hierarchy
-example by implementing polymorphism. Define
-a base class Shape with methods to calculate 
-area and perimeter. Then, create derived classes
-like Circle, Rectangle, and Triangle, each with its
- own implementation of these methods.*/
